e82f79fea5897be852ef9656128a483aa008bc21
abil0 <hoenabil6@gmail.com>
2026-03-14 22:58:51 +0700

    penambahan waktu live

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 
-function displayDate() {
+function updateTimeLive() {
     const el = document.getElementById("tanggal");
-    const d = new Date(); // D kapital
-    const options = { day: 'numeric', month: 'long', year: 'numeric' };
-    el.innerHTML = "EST. " + d.toLocaleDateString('id-ID', options);
+    
+    const d = new Date(); 
+    
+    const options = { 
+        weekday: 'long', 
+        day: 'numeric', 
+        month: 'long', 
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        timeZoneName: 'short'
+    };
+
+    el.innerHTML = "EST. " + d.toLocaleString('id-ID', options);
 }
-displayDate();
+
+setInterval(updateTimeLive, 1000);
+
+updateTimeLive();
 
 
 function tampil() {
     alert("🌟 COFFEBILLS EXCLUSIVE 🌟\n\nSelamat! Anda diundang menjadi member premium. Dapatkan akses ke CEO Lounge setiap hari!");
 }
 
-
 function show() {
-    alert("OFFICIAL FOUNDER\n\nNama: Nabil Rafif\nNIM: 20240140062\n\n'Success is brewed with hard work and a lot of caffeine.'");
+    alert("OFFICIAL FOUNDER\n\nNama: Nabil Rafif\nNIM: 20250140062\n\n'Success is brewed with hard work and a lot of caffeine.'");
 }
